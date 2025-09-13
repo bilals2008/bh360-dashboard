d587540ddc90250172f39f66d631a4c53592d952
bilals2008 <wbiz2939@gmail.com>
2025-09-13 12:49:57 +0500

    fix: update import path for useChat to match naming convention in GroupChats and ChatHeader components

--- src/dashboard/message/chat/chat-list/chat-types/GroupChats.jsx
+++ src/dashboard/message/chat/chat-list/chat-types/GroupChats.jsx
@@ -2,7 +2,7 @@
 import { CheckCheck, Hash } from "lucide-react";
 import React from "react";
 import { groupChatsData } from "../../../../../data/chatData";
-import { useChat } from "../../../../../context/ChatContext";
+import { useChat } from "../../../../../context/chatContext";
 
 function GroupChats() {
   const { selectChat } = useChat();
